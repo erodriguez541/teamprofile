a73851bd4f7b7d01f4f93bf12b4760c757eb22f0
ErnieRodriguez <erod541@gmail.com>
2021-05-22 23:17:25 -0500

    fixing const calls to inquire

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const inquirer = require('inquirer')
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
 
 class Employee {
     constructor(name, id, email) {
